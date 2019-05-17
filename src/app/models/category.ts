5e6ce5a68c0898e34d6c2d00d84da999a5f90aaa
Daniel Kloss <arsenal_fan_dan@hotmail.com>
2019-05-17 19:26:32 +0100

    Colours again

--- src/app/models/category.ts
+++ src/app/models/category.ts
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 import { Team } from "./team";
 
 export class Category {
 
     public name: string;
     public teams: Team[];
-=======
-export class Category {
-
-    public name: string;
-    public items: string[];
->>>>>>> e0c723bf6b58469b7e45758755af840a0914b137
 
     constructor(name: string) {
         this.name = name;
--- src/app/models/match.ts
+++ src/app/models/match.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Team } from "./team";
 
 export class Match {
@@ -9,15 +8,5 @@ export class Match {
     constructor(hometeam: Team, awayTeam: Team) {
         this.home = hometeam;
         this.away = awayTeam;
-=======
-export class Match {
-
-    public teamOne: string;
-    public teamTwo: string;
-
-    constructor(teamOne: string, teamTwo) {
-        this.teamOne = teamOne;
-        this.teamTwo = teamTwo;
->>>>>>> e0c723bf6b58469b7e45758755af840a0914b137
     }
 }

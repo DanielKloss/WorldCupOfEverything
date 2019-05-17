4d4c8bf3a66a234ae6561a653e11f6386a0c00f0
Daniel Kloss <arsenal_fan_dan@hotmail.com>
2019-05-17 19:22:39 +0100

    Colours

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
-=======
->>>>>>> e0c723bf6b58469b7e45758755af840a0914b137
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,14 +23,10 @@ import { ClientComponent } from './client/client.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-<<<<<<< HEAD
     FormsModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatListModule
-=======
-    FormsModule
->>>>>>> e0c723bf6b58469b7e45758755af840a0914b137
   ],
   providers: [],
   bootstrap: [AppComponent]
